eeacb6b2ee3e1108583556e5b590f08fb24548d6
nunopenim <nuno.penim@gmail.com>
2020-03-16 10:48:55 +0000

    init: heroku environment var check added

--- tg_bot/__init__.py
+++ tg_bot/__init__.py
@@ -59,6 +59,7 @@ if ENV:
     ALLOW_EXCL = os.environ.get('ALLOW_EXCL', False)
     API_WEATHER = os.environ.get('API_OPENWEATHER', None)
     STRICT_GMUTE = bool(os.environ.get('STRICT_GMUTE', False))
+    HEROKU = True
 
 else:
     from tg_bot.config import Development as Config
@@ -102,6 +103,7 @@ else:
     ALLOW_EXCL = Config.ALLOW_EXCL
     API_WEATHER = Config.API_OPENWEATHER
     STRICT_GMUTE = Config.STRICT_GMUTE
+    HEROKU = False
 
 SUDO_USERS.add(OWNER_ID)
 
